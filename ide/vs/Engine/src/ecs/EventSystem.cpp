69f92fe5cdcd02677c55a191454aaa5725b32226
Jun <jzhi@gaming.tech>
2024-06-06 01:44:32 +0200

    [ADD] EventSystem.cpp

---
+++ ide/vs/Engine/src/ecs/EventSystem.cpp
@@ -0,0 +1,18 @@
+#include "EventSystem.h"
+
+void GCEventSystem::PollEvents()
+{
+}
+
+void GCEventSystem::AddEventListener()
+{
+}
+
+void GCEventSystem::RemoveEventListener()
+{
+}
+
+void GCEventSystem::OnEvent(GCEvent& e)
+{
+    //TODO: Dispatch different event to listeners
+}
