527687c3654c65b597938f969ef95770fa3ab216
Mateo Lemercier <mlemercier@gaming.tech>
2024-06-05 10:31:21 +0200

    [ADD] New Components
    Liste des components ajoutés :
    - SpriteRenderer
    - Collider (base pour tous les colliders)
    - BoxCollider
    - CircleCollider
    - RigidBody
    - Animator
    - SoundMixer
    - Script

--- ide/vs/Engine/Components.h
+++ ide/vs/Engine/Components.h
@@ -14,3 +14,110 @@ public:
 
 };
 
+
+
+class SpriteRenderer : public Component
+{
+
+public:
+    ~SpriteRenderer() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class Collider : public Component
+{
+
+public:
+    ~Collider() override {};
+
+};
+
+
+
+class BoxCollider : public Collider
+{
+
+public:
+    ~BoxCollider() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class CircleCollider : public Collider
+{
+
+public:
+    ~CircleCollider() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class RigidBody : public Component
+{
+
+public:
+    ~RigidBody() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class Animator : public Component
+{
+
+public:
+    ~Animator() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class SoundMixer : public Component
+{
+
+public:
+    ~SoundMixer() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
+
+
+
+class Script : public Component
+{
+
+public:
+    ~Script() override {};
+    
+    void Init() override {};
+    void Update() override {};
+    void Destroy() override {};
+
+};
