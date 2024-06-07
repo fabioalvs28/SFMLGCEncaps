a6553da01cc2f7a35098604638fc19a8836f15b3
Kaktousse <elia.maillardsalin@gmail.com>
2024-06-07 15:49:53 +0200

    [REF] Update avec le Scene Manager
    connecter le scene manager au gameobject et game manager.

--- ide/vs/Engine/GameManager.h
+++ ide/vs/Engine/GameManager.h
@@ -1,21 +1,20 @@
 #pragma once
-#include "../Core/framework.h"
 
 class GameObject;
+
+class SceneManager; 
 
 class GameManager 
 {
 
 public:
+
     GameManager() {};
     virtual ~GameManager() {};
     
     void Init();
     void Update();
     
-    GameObject* CreateGameObject( const char* name/* = GameObject */, bool active/* = true */);
-
 private:
-    GCLinkedList<GameObject*> m_gameObjectsList;
-
+    SceneManager* m_sceneManager; 
 };
