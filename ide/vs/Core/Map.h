12529c0206f49d417ad75c1fb164ad681bb62ba7
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2024-06-11 12:10:53 +0200

    [FIX] Get method (i forgot to save)

--- ide/vs/Core/Map.h
+++ ide/vs/Core/Map.h
@@ -13,7 +13,7 @@ public:
 	void Insert(const Key& key, const Value& value);
 	void Remove(const Key& key);
 	bool Find(const Key& key, Value& value);
-	Value& GetValue(const Key& key) const;
+	const Value& GetValue(const Key& key) const;
 	void Clear();
 	size_t GetSize() const;
 	bool IsEmpty() const;
@@ -49,12 +49,11 @@ bool GCMap<Key, Value>::Find(const Key& key, Value& value)
 }
 
 template<typename Key, typename Value>
-Value& GCMap<Key, Value>::GetValue(const Key& key) const
+const Value& GCMap<Key, Value>::GetValue(const Key& key) const
 {
 	auto it = m_Map.find(key);
 	assert(it != m_Map.end());
-	value = it->second;
-	return value;
+	return it->second;
 }
 
 template<typename Key, typename Value>
