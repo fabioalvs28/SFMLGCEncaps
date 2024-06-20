bc43b8d01a2addcd55a13a5697bb2e278a98ebc2
Jun <jzhi@gaming.tech>
2024-06-20 10:41:39 +0200

    [REF] Window class is abstract now

--- src/engine/Window.h
+++ src/engine/Window.h
@@ -1,20 +1,40 @@
 #pragma once
 #include <Windows.h>
-#include "EventSystem.h"
+#include "Event.h"
+
+struct GCWindowProperties
+{
+    const wchar_t* Title;
+    int32_t Width = 0, Height = 0;
+
+    GCWindowProperties(const wchar_t* title = L"GC Engine", int32_t width = 1600, int32_t height = 800)
+        : Title(title), Width(width), Height(height)
+    {
+    }
+};
+
+using GCEventCallback = std::function<void(GCEvent& ev)>;
 
 class GCWindow
 {
 public:
-    GCWindow();
-    ~GCWindow();
+    GCWindow(const GCWindowProperties& properties)
+        : m_properties(properties) {}
 
-    bool Create();
-    void Show();
-    void PollEvents();
+    virtual ~GCWindow() = default;
+
+    virtual void SetWindowCallBack(const GCEventCallback) = 0;
+
+    virtual void OnUpdate() = 0;
+
+    int32_t GetWidth() const { return m_properties.Width; }
+    int32_t GetHeight() const { return m_properties.Height; }
+
 
 private:
-	void EnableVirtualTerminalProcessing();
+	void EnableVirtualTerminalProcessing(); //TODO: Move it to Testing.proj
 
 private:
-
+    GCWindowProperties m_properties;
+    GCEventCallback m_windowCallback = nullptr;
 };
