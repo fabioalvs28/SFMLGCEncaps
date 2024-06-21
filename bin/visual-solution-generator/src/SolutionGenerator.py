58656ed975cbe0b35043827a5eda46efe70e1e13
William <williamchapronpro@gmail.com>
2024-06-21 10:17:05 +0200

    [MERGE] Render-WilliamChapron On Render-Principal
    [MERGE] Render-WilliamChapron On Render-Principal

--- bin/visual-solution-generator/src/SolutionGenerator.py
+++ bin/visual-solution-generator/src/SolutionGenerator.py
@@ -169,7 +169,7 @@ def generate_vcxproj(project):
     property_group = ET.SubElement(root, "PropertyGroup", Label="Globals")
     add_text_element(property_group, "VCProjectVersion", project.get("vc_project_version"))
     add_text_element(property_group, "Keyword", "Win32Proj")
-    add_text_element(property_group, "ProjectGuid", f"{{{project['guid']}}})")
+    add_text_element(property_group, "ProjectGuid", f"{{{project['guid']}}}")
     add_text_element(property_group, "RootNamespace", project.get("root_namespace"))
     add_text_element(property_group, "WindowsTargetPlatformVersion", project.get("windows_target_platform_version"))
 
@@ -292,14 +292,32 @@ def populate_include_files(project):
     else:
         os.makedirs(src_path + project['folder'])
 
-def generate_solution(data):
-    
-    # Remove the ide folder
-    if os.path.exists(ide_path):
+def delete_folder_safe(folder_path):
+    ext = tuple((src_ext + ";" + h_ext + ";" + rc_ext).split(";"))
+    exclude_dirs = [".vs", "Build"]
+    for root, dirs, files in os.walk(folder_path):
+        has_src_files = False
+        
+        for dir in dirs:
+            if not dir in exclude_dirs:
+                has_src_files = delete_folder_safe(root + dir + "/")
+                if has_src_files:
+                    return True
+        
+        for file in files:
+            if os.path.splitext(file)[1].lstrip('.') in ext:
+                print(f"WARNING: source file '{file}' is present in the folder '{root}'. It won't be deleted.")
+                return True
+        
         try:
-            shutil.rmtree(ide_path)
+            shutil.rmtree(root)
         except Exception as e:
-            print(f"An error occurred while deleting the ide folder: {e}")
+            # print(f"An error occurred while deleting the ide folder: {e}")
+            continue
+
+def generate_solution(data):
+    # Remove the ide folder
+    delete_folder_safe(ide_path)
 
     # Generate the solution
     generate_sln(data)
