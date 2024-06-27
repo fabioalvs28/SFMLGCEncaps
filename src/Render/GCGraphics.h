a541a8f794be818ef299f939d527bc19b63f8a15
Maquereauu <macrorush@gmail.com>
2024-06-27 10:20:32 +0200

    [DOC] added comments

--- src/Render/GCGraphics.h
+++ src/Render/GCGraphics.h
@@ -13,22 +13,49 @@ public:
 	~GCGraphics();
 
 	/**
-* Initializes graphics.
- * @brief
- *
- * This function takes into account the window you want to use as well as sizes you want your render to be(not the window,the actual renderer screen).
- *
- * @param Window that'll be used.
- * @param render-Window's desired width(preferably 1920 or lower).
- * @param render-Window's desired height(preferably 1080 or lower).
- */
+	* Initializes graphics.
+	 * @brief
+	 *
+	 * This function takes into account the window you want to use as well as sizes you want your render to be(not the window,the actual renderer screen).
+	 *
+	 * @param Window that'll be used.
+	 * @param render-Window's desired width(preferably 1920 or lower).
+	 * @param render-Window's desired height(preferably 1080 or lower).
+	 */
 	void Initialize(Window* pWindow, int renderWidth, int renderHeight);
 
 	// Each Frame
+		/**
+	* Call it at the beginning of a frame.
+	 * @brief
+	 *
+	 * This function contains the prepareDraw func.
+	 *
+	 */
 	void StartFrame();
+	/**
+	* Call it at the end of a frame.
+	 * @brief
+	 *
+	 * This function contains the postDraw func.
+	 *
+	 */
 	void EndFrame();
-
+	/**
+	* Call it before initializing ressources.
+	 * @brief
+	 *
+	 * This function resets the command list so that you can create ressources,don't forget to call InitializeGraphicsResourcesEnd after your last initialization.
+	 *
+	 */
 	void InitializeGraphicsResourcesStart();
+	/**
+	* Call it after initializing ressources.
+	 * @brief
+	 *
+	 * This function executes what's inside the command list ,don't forget to call InitializeGraphicsResourcesStart before.
+	 *
+	 */
 	void InitializeGraphicsResourcesEnd();
 
 	// Shader
@@ -65,16 +92,50 @@ public:
 	 * @return world matrix of the object at said coordinates(pixelX/pixelY)
 	 */
 	DirectX::XMFLOAT4X4 ToPixel(int pixelX, int pixelY, DirectX::XMFLOAT4X4 proj, DirectX::XMFLOAT4X4 view);
-
+	/**
+	 * Creates a material using a shader.
+	 * @brief
+	 *
+	 *
+	 * @param shader that'll be used by the material
+	 *
+	 * @return Material using said shader
+	 */
 	ResourceCreationResult<GCMaterial*> CreateMaterial(GCShader* pShader);
-
+	/**
+	 * Creates a mesh using a geometry.
+	 * @brief
+	 *
+	 *
+	 * @param geometry that'll be used by the mesh
+	 *
+	 * @return Mesh using said geometry
+	 */
 	ResourceCreationResult<GCMesh*> CreateMesh(GCGeometry* pGeometry);
-
+	/**
+	 * Creates a Geometry using a template that's using UVs.
+	 * @brief
+	 *
+	 *
+	 * @param name of the primitive(that will later be changed to an int using an enum I suppose?)
+	 *
+	 * @return Geometry using UVs created using a template
+	 */
 	ResourceCreationResult<GCGeometry*> CreateGeometryPrimitiveTexture(const std::string& primitiveName);
+	/**
+	 * Creates a Geometry using a template that's using colors.
+	 * @brief
+	 *
+	 *
+	 * @param name of the primitive(that will later be changed to an int using an enum I suppose?)
+	 * @param color in which the object will be drawn
+	 *
+	 * @return Geometry using colors created using a template
+	 */
 	ResourceCreationResult<GCGeometry*> CreateGeometryPrimitiveColor(const std::string& primitiveName, const DirectX::XMFLOAT4& color);
 	ResourceCreationResult<GCGeometry*> CreateGeometryPrimitiveCustom(const std::string& primitiveName, const DirectX::XMFLOAT4& color, int& flagEnabledBits);
 	/**
-	 * Creates Geometry using a file that's using UVs.
+	 * Creates a Geometry using a file that's using UVs.
 	 * @brief
 	 *
 	 *
@@ -85,7 +146,7 @@ public:
 	 */
 	ResourceCreationResult<GCGeometry*> CreateGeometryModelParserTexture(const std::string& filePath, Extensions fileExtensionType);
 	/**
-	 * Creates Geometry using a file that's using colors.
+	 * Creates a Geometry using a file that's using colors.
 	 * @brief
 	 *
 	 *
@@ -125,6 +186,7 @@ public:
 
 	// Remove using active flags
 	bool RemoveMesh(GCMesh* pMesh);
+	// Remove using active flags
 	bool RemoveTexture(GCTexture* pTexture);
 
 	std::vector<GCShader*> GetShaders();
