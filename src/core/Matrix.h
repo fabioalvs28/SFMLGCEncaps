d69d903b67dd5f208c8bfa2c47431844819ddd7f
Mateo Lemercier <mlemercier@gaming.tech>
2024-07-08 12:01:15 +0200

    [ADD] Matrix.IsIdentity()

--- src/core/Matrix.h
+++ src/core/Matrix.h
@@ -33,5 +33,7 @@ struct GCMATRIX
 	void Translation(float x, float y, float z); //translation la matrice
 	void Rotation(GCVEC3 position, float angle); //rotation la matrice
 	void Scale(float x, float y, float z); //scale la matrice
+	
+	bool IsIdentity() const; //verifie si la matrice est l'identite
 
 };
