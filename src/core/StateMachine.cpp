0cc6ff1c17ba89e07f5f8404adaed26594db7a56
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2024-07-16 10:07:58 +0200

    [FIX] Use forward declaration instead of include

--- src/core/StateMachine.cpp
+++ src/core/StateMachine.cpp
@@ -1,6 +1,6 @@
 #include "pch.h"
 #include "StateMachine.h"
-
+#include "State.h"
 
 GCStateMachine::GCStateMachine() : m_pCurrentState(nullptr) {}
 
--- src/core/StateMachine.h
+++ src/core/StateMachine.h
@@ -1,5 +1,5 @@
 #pragma once 
-#include "State.h"
+class GCState;
 
 class GCStateMachine
 {
