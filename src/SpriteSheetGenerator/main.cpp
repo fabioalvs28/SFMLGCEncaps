2213bcb46b9ece469959399cde61aca426dbe400
OshidaBCF <swann.sanchez9@outlook.fr>
2024-07-17 10:54:26 +0200

    [ADD] More code

--- src/SpriteSheetGenerator/main.cpp
+++ src/SpriteSheetGenerator/main.cpp
@@ -1,13 +1,14 @@
 #include "math.h"
 #include "GCImage.h"
-#include "GCFile.h"
 #include "string"
+#include <iostream>
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
+namespace fs = std::filesystem;
 
 struct Sprite
 {
-    GCImage image;
+    //GCImage image;
     int x;
     int y;
     int w;
@@ -58,21 +59,25 @@ static int getSizeOfImageList(std::vector<Sprite> Sprites, int padding)
     return total;
 }
 
-int number_of_files_in_directory(std::filesystem::path path)
+static int number_of_files_in_directory(fs::path path)
 {
     int count = 0;
 
-    for (auto& p : std::filesystem::directory_iterator(path))
+    for (auto& p : fs::directory_iterator(path))
     {
         ++count;
     }
     return count;
 }
 
+
+#undef main
+
+
 int main()
 {
-    std::filesystem::path importPath = "C:\\Users\\ssanchez\\Downloads\\SpriteSheetGenerator-py\\randomImage\\";
-    std::filesystem::path outputPath = "C:\\Users\\ssanchez\\Downloads\\SpriteSheetGenerator-py\\output\\";
+    fs::path importPath = "C:\\Users\\ssanchez\\Downloads\\SpriteSheetGenerator-py\\randomImage\\";
+    fs::path outputPath = "C:\\Users\\ssanchez\\Downloads\\SpriteSheetGenerator-py\\output\\";
 
     json data = json::parse(R"({})");
     data["totalImageCount"] = 0;
@@ -80,38 +85,66 @@ int main()
     data["textures"] = json::array();
     data["spriteIndex"] = json::object();
 
-    bool importPathExists = std::filesystem::is_directory(importPath.parent_path());
+    bool importPathExists = fs::is_directory(importPath.parent_path());
     if (!importPathExists)
     {
         std::printf("Error : Import Path isn't a folder\n");
-        //sys.exit(1);
+        return 1;
     }
 
-    bool outputPathExists = std::filesystem::exists(outputPath.parent_path());
+    bool outputPathExists = fs::exists(outputPath.parent_path());
     if (outputPathExists)
     {
-        if (!std::filesystem::is_directory(outputPath.parent_path()))
+        if (!fs::is_directory(outputPath.parent_path()))
         {
             std::printf("Error : Output Path isn't a folder\n");
-            //sys.exit(1);
+            return 1;
         }
     }
     else
     {
-        std::filesystem::create_directory(outputPath);
+        fs::create_directory(outputPath);
     }
 
+
+    if (number_of_files_in_directory(importPath) == 0)
+    {
+        std::printf("Error : Input Path is empty\n");
+        return 1;
+    }
     
     if (number_of_files_in_directory(outputPath) > 0)
     {
         std::printf("Error : Output Path isn't empty\n");
-        //sys.exit(1);
+        return 1;
     }
 
+    std::vector<std::string> fileList;
+
     //fileList = sorted_alphanumeric(os.listdir(importPath));
-    
+    for (const auto& entry : fs::directory_iterator(importPath))
+    {
+        if (entry.is_regular_file() && entry.path().extension() == ".png")
+        {
+            std::string filename = entry.path().filename().string();
+            data["spriteIndex"][filename] = "";
+            auto imagefile = GCFile(entry.path().string().c_str(), "r");
+            //GCImage::Load(imagefile, sizeof(imagefile))
+            /*image = Image.open(importPath + filename)
+            w, h = image.size
+            rotated = False
+            if w > h and allowRotate:
+                image = image.rotate(90, expand = True)
+                w, h = image.size
+                rotated = True
+            imageList.append((image.copy(), w, h, filename, rotated)) #filename.split('.')[0]
+            totalSize += (w + padding * 2) * (h + padding * 2)*/
+        }
+    }
+
+
     std::vector<Sprite> imageList;
     int totalSize = 0;
 
-    return 1;
+    return 0;
 }
