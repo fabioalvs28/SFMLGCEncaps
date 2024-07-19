cbbfa047228c6659e77b0a4467d3a9a6fb4f8adb
ArthurChassaing <94675172+ArthurChassaing@users.noreply.github.com>
2024-07-19 09:37:29 +0200

    [REF] Rework time class

--- src/engine/Timer.cpp
+++ src/engine/Timer.cpp
@@ -2,93 +2,143 @@
 
 #include "Timer.h"
 
-#define FIXED_UPDATE 0.2
-
-Timer::Timer()
+GCTime::GCTime()
 {
-    Init();
+	Reset();
 }
 
-Timer::~Timer() {
-}
-
-void Timer::Init()
+GCTime::~GCTime()
 {
-    m_previousTime = 0;
-    m_deltaTime = -1.0;
-    m_totalTime = 0;
-    m_timeScale = 1;
-    m_timeScaleBackup = m_timeScale;
-    m_MaxDeltaTime = 0;
-
-    m_StartTime = std::chrono::steady_clock::now();
-    m_InitTime = std::chrono::steady_clock::now();
 }
 
-void Timer::Reset()
+void GCTime::Reset()
 {
-    Init();
+	// System
+	m_initTime = timeGetTime();
+	m_previousTime = m_initTime;
+	m_isPerformanceTimer = false;
+	m_frequency = 0.0f;
+	LARGE_INTEGER frequency;
+	memset(&frequency, 0, sizeof(LARGE_INTEGER));
+	if (QueryPerformanceFrequency(&frequency) && frequency.QuadPart)
+	{
+		m_isPerformanceTimer = true;
+		m_frequency = (float)frequency.QuadPart;
+		LARGE_INTEGER counter;
+		QueryPerformanceCounter(&counter);
+		m_performTime = counter.QuadPart;
+		m_previousTime = (float)(counter.QuadPart - m_performTime) / m_frequency;
+	}
+	m_sysDeltaTime = 0.0f;
+
+	// Properties
+	m_maxDeltaTime = m_defMaxDeltaTime;
+	m_timeScale = 1.0f;
+	m_running = true;
+
+	// Time
+	m_totalTime = 0.0f;
+	m_deltaTime = 0.0f;
+
+	// FPS
+	m_fpsElapsed = 0.0f;
+	m_fpsCount = 0;
+	m_fps = 0;
 }
 
-void Timer::Start()
+void GCTime::Start()
 {
-    m_timeScale = m_timeScaleBackup;
+	m_running = true;
 }
 
-void Timer::Stop()
+void GCTime::Stop()
 {
-    if (m_timeScale == 0)
-        return;
-
-    m_timeScaleBackup = m_timeScale;
-    m_timeScale = 0;
+	m_running = false;
 }
 
-void Timer::Tick()
+bool GCTime::Update()
 {
-    std::chrono::duration<float> totalTimeDuration = std::chrono::steady_clock::now() - m_InitTime;
-    
-    m_totalTime = totalTimeDuration.count();
+	// Performance or Classic
+	float cur = 0.0f;
+	if (m_isPerformanceTimer)
+	{
+		LARGE_INTEGER counter;
+		QueryPerformanceCounter(&counter);
+		cur = (float)(counter.QuadPart - m_performTime) / m_frequency;
+	}
+	else
+		cur = (timeGetTime() - m_initTime) / 1000.0f;
 
-    m_previousTime = m_deltaTime;
+	// System delta time
+	float dt = cur - m_previousTime;
+	m_previousTime = cur;
 
-    std::chrono::duration<float> duration = std::chrono::steady_clock::now() - m_StartTime;
-    
-    m_deltaTime = duration.count() * m_timeScale;
-    if (m_MaxDeltaTime > 0 && m_deltaTime > m_MaxDeltaTime)
-        m_deltaTime = m_MaxDeltaTime;
-    
-    m_FPS = m_deltaTime != 0  ? 1 / m_deltaTime : -1;
+	// Pause
+	if (m_running == false)
+	{
+		m_sysDeltaTime = 0.0f;
+		m_deltaTime = 0.0f;
+		return true;
+	}
 
-    m_StartTime = std::chrono::steady_clock::now();
+	// System delta time
+	m_sysDeltaTime += dt;
+	if (m_sysDeltaTime < m_defMinDeltaTime)
+	{
+		m_deltaTime = 0.0f;
+		return false;
+	}
+	if (m_sysDeltaTime > m_maxDeltaTime)
+		m_sysDeltaTime = m_maxDeltaTime;
+
+	// FPS
+	m_fpsCount++;
+	m_fpsElapsed += m_sysDeltaTime;
+	if (m_fpsElapsed >= 1.0f)
+	{
+		m_fps = m_fpsCount;
+		m_fpsCount = 0;
+		m_fpsElapsed = m_fpsElapsed - 1.0f;
+	}
+
+	// Delta time
+	m_deltaTime = m_sysDeltaTime;
+	m_deltaTime *= m_timeScale;
+	m_sysDeltaTime = 0.0f;
+
+	// Total time
+	m_totalTime += m_deltaTime;
+
+	// Ok
+	return true;
 }
 
-void Timer::SetMaxDeltaTime(float maxDeltaTime)
+void GCTime::SetMaxDeltaTime(float maxDeltaTime)
 {
-    m_MaxDeltaTime = maxDeltaTime;
+	m_maxDeltaTime = maxDeltaTime;
 }
 
-void Timer::SetTimeScale(float timeScale) 
+void GCTime::SetTimeScale(float timeScale)
 {
-    m_timeScale = timeScale;
+	m_timeScale = timeScale;
 }
 
-float Timer::DeltaTime() const
+float GCTime::DeltaTime() const
 {
-    return m_deltaTime;
+	return m_deltaTime;
 }
 
-float Timer::FixedDeltaTime() const
+float GCTime::FixedDeltaTime() const
 {
-    return FIXED_UPDATE;
+	return m_fixedDeltaTime;
 }
 
-float Timer::TotalTime() const
+float GCTime::TotalTime() const
 {
-    return m_totalTime;
+	return m_totalTime;
 }
 
-float Timer::GetFPS() const
-{   
-    return m_FPS;
+float GCTime::GetFPS() const
+{
+	return m_fps;
 }
