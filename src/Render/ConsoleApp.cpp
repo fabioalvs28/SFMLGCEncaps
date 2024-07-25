d989e5f0e5421376d4330f4f7a5f39e0d6f742ed
William <williamchapronpro@gmail.com>
2024-07-25 16:27:54 +0200

    [ADD] All Light Work On 3D Deferred Light Shading + Need Fix Reconstruct 3d pos + DSL Doesn't work correctly in 2D + Need To change Gbuffer Format
    [ADD] All Light Work On 3D Deferred Light Shading + Need Fix Reconstruct 3d pos + DSL Doesn't work correctly in 2D + Need To change Gbuffer Format

--- src/Render/ConsoleApp.cpp
+++ src/Render/ConsoleApp.cpp
@@ -1,4 +1,4 @@
-﻿//// 2D BASIC
+﻿//// 2D ALPHABET
 //
 //#include "pch.h"
 //
@@ -177,10 +177,7 @@ XMMATRIX CreateBillboardMatrix(XMVECTOR objectPosition, XMVECTOR cameraPosition,
     return XMMatrixTranspose(billboardMatrix);
 }
 
-// D�finition des variables globales pour la cam�ra
-XMVECTOR cameraPosition = XMVectorSet(0.0f, -10.0f, 5.0f, 1.0f);
-XMVECTOR cameraTarget = XMVectorZero();
-XMVECTOR cameraUp = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+
 
 float cameraMoveSpeed = 0.05f;
 
@@ -278,7 +275,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
         aspectRatio,
         nearZ,
         farZ,
-        PERSPECTIVE, 
+        20.0f,
+        20.0f,
+        PERSPECTIVE,
         storedProjectionMatrix,
         storedViewMatrix
     );
@@ -423,9 +422,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 
 
 
-
-// 2D DEFERRED SHADING 
-
+//
+//// 2D DEFERRED SHADING
+//
 //#include "pch.h"
 //
 //using namespace DirectX;
@@ -445,8 +444,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //}
 //
 //// D�finition des variables globales pour la cam�ra
-//XMVECTOR cameraPosition = XMVectorSet(0.0f, 0.0f, -10.0f, 1.0f);
-//XMVECTOR cameraTarget = XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
+//XMVECTOR cameraPosition = XMVectorSet(0.0f, -10.0f, 5.0f, 1.0f);
+//XMVECTOR cameraTarget = XMVectorZero();
 //XMVECTOR cameraUp = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 //
 //float cameraMoveSpeed = 0.05f;
@@ -459,12 +458,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //    Window* window = new Window(hInstance);
 //    window->Initialize();
 //
+//    std::string PPFilePath = "../../../src/Render/Shaders/test.hlsl";
+//    std::string PPcsoDestinationPath = "../../../src/Render/CsoCompiled/test";
+//
 //    GCGraphics* graphics = new GCGraphics();
 //    graphics->Initialize(window, 1920, 1080);
 //
-//    //graphics->GetRender()->ActiveBasicPostProcessing();
-//    //graphics->GetRender()->ActivePixelIDMapping();
-//    graphics->GetRender()->ActiveDeferredLightPass();
+//    graphics->GetRender()->CreatePostProcessingResources("../../../src/Render/Shaders/test.hlsl", "../../../src/Render/CsoCompiled/test");
+//    //graphics->GetRender()->ActiveCSPostProcessing();
+//    //graphics->GetRender()->ActiveDeferredLightPass();
 //
 //    int flagsLightColor = 0;
 //    SET_FLAG(flagsLightColor, VERTEX_POSITION);
@@ -478,14 +480,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //
 //    auto geometryPostProcessing = graphics->CreateGeometryPrimitive(Quad, DirectX::XMFLOAT4(DirectX::Colors::Yellow));
 //    auto geoCubeOuter = graphics->CreateGeometryPrimitive(CubeSkybox, XMFLOAT4(Colors::Red));
-//    auto geoCubeInner = graphics->CreateGeometryPrimitive(Plane, XMFLOAT4(Colors::Green));
+//    auto geoCubeInner = graphics->CreateGeometryPrimitive(Cube, XMFLOAT4(Colors::Green));
 //    auto geoSphere = graphics->CreateGeometryPrimitive(Plane, XMFLOAT4(Colors::Yellow));
 //
 //    std::string shaderFilePath1 = "../../../src/Render/Shaders/LightColor.hlsl";
 //    std::string csoDestinationPath1 = "../../../src/Render/CsoCompiled/LightColor";
 //
-//    std::string shaderFilePath3 = "../../../src/Render/Shaders/colorDeferredLight.hlsl";
-//    std::string csoDestinationPath3 = "../../../src/Render/CsoCompiled/colorDeferredLight";
+//    std::string shaderFilePath3 = "../../../src/Render/Shaders/LightColor.hlsl";
+//    std::string csoDestinationPath3 = "../../../src/Render/CsoCompiled/LightColor";
 //
 //    auto shaderLightColor = graphics->CreateShaderCustom(shaderFilePath3, csoDestinationPath3, flagsLightColor);
 //
@@ -508,6 +510,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //    auto meshCubeInner = graphics->CreateMeshCustom(geoCubeInner.resource, flagsLightColor);
 //    auto meshSphere = graphics->CreateMeshCustom(geoSphere.resource, flagsLightColor);
 //
+//    //meshCubeInner.resource->AddGeometry(geoCubeInner.resource, XMFLOAT3(1.0f, 1.0f, 1.0f));
+//    std::string texturePath = "../../../src/Render/Textures/texture.dds";
+//    std::string texturePath2 = "../../../src/Render/Textures/cottage_diffuse.dds";
+//    auto texture = graphics->CreateTexture(texturePath);
+//    auto texture2 = graphics->CreateTexture(texturePath2);
 //
 //    graphics->InitializeGraphicsResourcesEnd();
 //
@@ -516,21 +523,75 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //    //materialCubeOuter.resource->SetTexture(texture2.resource);
 //
 //    auto materialCubeInner = graphics->CreateMaterial(shaderLightColor.resource);
-//    auto materialSphere = graphics->CreateMaterial(shaderLightColor.resource);
+//    auto materialCubeInner2 = graphics->CreateMaterial(shaderLightColor.resource);
+//    auto materialSphere = graphics->CreateMaterial(shaderLightTexture.resource);
+//    materialSphere.resource->SetTexture(texture.resource);
 //
-//    float viewWidth = 20.0f;
-//    float viewHeight = 20.0f;
-//    XMMATRIX projectionMatrix = XMMatrixOrthographicLH(viewWidth, viewHeight, 1.0f, 1000.0f);
-//    XMMATRIX viewMatrix = XMMatrixLookAtLH(cameraPosition, cameraTarget, cameraUp);
-//    XMMATRIX transposedProjectionMatrix = XMMatrixTranspose(projectionMatrix);
-//    XMMATRIX transposedViewMatrix = XMMatrixTranspose(viewMatrix);
-//    GCMATRIX storedProjectionMatrix = GCUtils::XMMATRIXToGCMATRIX(transposedProjectionMatrix);
-//    GCMATRIX storedViewMatrix = GCUtils::XMMATRIXToGCMATRIX(transposedViewMatrix);
 //
-//    XMMATRIX worldMatrixCubeOuter = XMMatrixScaling(10.0f, 10.0f, 10.0f) * XMMatrixTranslation(0.0f, -3.0f, 0.0f); // Cube externe (skybox)
-//    XMMATRIX worldMatrixCubeInner = XMMatrixScaling(20.0f, 20.0f, 20.0f) * XMMatrixTranslation(0.0f, 0.0f, 0.0f); // Cube interne centr�
-//    XMMATRIX worldMatrixCubeInner2 = XMMatrixScaling(3.0f, 3.0f, 3.0f) * XMMatrixTranslation(3.0f, 0.0f, 0.0f); // Cube interne centr�
-//    XMMATRIX worldMatrixSphere = XMMatrixScaling(2.0f, 2.0f, 2.0f) * XMMatrixTranslation(3.0f, 5.0f, 0.0f); // Sph�re d�plac�e dans le cube interne
+//    // PERSPECTIVE 
+//    
+//    //GCVEC3 cameraPosition(0.0f, -10.0f, 5.0f);
+//    //GCVEC3 cameraTarget(0.0f, 0.0f, 0.0f);
+//    //GCVEC3 cameraUp(0.0f, 1.0f, 0.0f);
+//    //float fieldOfView = 0.25f * XM_PI; // Exemple de champ de vision pour la perspective
+//    //float aspectRatio = window->AspectRatio(); // Assurez-vous que `window` est défini
+//    //float nearZ = 1.0f;
+//    //float farZ = 1000.0f;
+//    //float viewWidth = 20.0f;
+//    //float viewHeight = 20.0f;
+//
+//    //GCMATRIX storedProjectionMatrix, storedViewMatrix;
+//    //graphics->CreateViewProjConstantBuffer(
+//    //    cameraPosition,
+//    //    cameraTarget,
+//    //    cameraUp,
+//    //    fieldOfView,
+//    //    aspectRatio,
+//    //    nearZ,
+//    //    farZ,
+//    //    viewWidth,
+//    //    viewHeight,
+//    //    PERSPECTIVE,
+//    //    storedProjectionMatrix,
+//    //    storedViewMatrix
+//    //);
+//
+//    // ORTHO
+//
+//    // Définir les paramètres de la caméra
+//    GCVEC3 cameraPosition(0.0f, 0.0f, -10.0f);
+//    GCVEC3 cameraTarget(0.0f, 0.0f, 0.0f);
+//    GCVEC3 cameraUp(0.0f, 1.0f, 0.0f);
+//
+//    // Paramètres pour la projection orthographique
+//    float viewWidth = 20.0f;   // Largeur de la vue (modifiez selon vos besoins)
+//    float viewHeight = 20.0f;  // Hauteur de la vue (modifiez selon vos besoins)
+//    float nearZ = 1.0f;        // Plan proche
+//    float farZ = 1000.0f;      // Plan éloigné
+//
+//    // Matrices pour stocker les résultats
+//    GCMATRIX storedProjectionMatrix, storedViewMatrix;
+//
+//    // Appeler la fonction pour créer les matrices de vue et de projection
+//    graphics->CreateViewProjConstantBuffer(
+//        cameraPosition,
+//        cameraTarget,
+//        cameraUp,
+//        0.0f,                 // Champ de vision non utilisé pour l'orthographique
+//        0.0f,                 // Rapport d'aspect non utilisé pour l'orthographique
+//        nearZ,
+//        farZ,
+//        viewWidth,
+//        viewHeight,
+//        ORTHOGRAPHIC,         // Type de projection
+//        storedProjectionMatrix,
+//        storedViewMatrix
+//    );
+//
+//    XMMATRIX worldMatrixCubeOuter = XMMatrixScaling(20.0f, 20.0f, 20.0f) * XMMatrixTranslation(0.0f, -3.0f, 0.0f); // Cube externe (skybox)
+//    XMMATRIX worldMatrixCubeInner = XMMatrixScaling(2.0f, 2.0f, 2.0f) * XMMatrixTranslation(0.0f, 10.0f, 0.0f); // Cube interne centr�
+//    XMMATRIX worldMatrixCubeInner2 = XMMatrixScaling(15.0f, 15.0f, 15.0f) * XMMatrixTranslation(0.0f, 0.0f, -1.0f); // Cube interne centr�
+//    XMMATRIX worldMatrixSphere = XMMatrixScaling(2.0f, 2.0f, 2.0f) * XMMatrixTranslation(3.0f, 5.0f, -2.0f); // Sph�re d�plac�e dans le cube interne
 //
 //    GCMATRIX worldCubeInner = GCUtils::XMMATRIXToGCMATRIX(worldMatrixCubeInner);
 //    GCMATRIX worldCubeInner2 = GCUtils::XMMATRIXToGCMATRIX(worldMatrixCubeInner2);
@@ -551,46 +612,66 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance, PSTR cmdLine, in
 //
 //        GCMATERIALPROPERTIES materialProperties;
 //        materialProperties.ambientLightColor = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
-//        materialProperties.ambient = DirectX::XMFLOAT4(0.2f, 0.2f, 0.2f, 1.0f);
+//        materialProperties.ambient = DirectX::XMFLOAT4(0.4f, 0.4f, 0.4f, 1.0f);
 //        materialProperties.diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
 //        materialProperties.specular = DirectX::XMFLOAT4(0.8f, 0.8f, 0.8f, 1.0f);
-//        materialProperties.shininess = 1.0f;
-//        graphics->UpdateMaterialProperties(materialCubeOuter.resource, materialProperties);
+//        materialProperties.shininess = 128.0f;
+//        graphics->UpdateMaterialProperties(materialCubeInner.resource, materialProperties);
 //
-//        GCLIGHTSPROPERTIES lightData = {};
+//        GCMATERIALPROPERTIES materialProperties2;
+//        materialProperties2.ambientLightColor = DirectX::XMFLOAT4(0.5f, 0.2f, 1.0f, 1.0f);
+//        materialProperties2.ambient = DirectX::XMFLOAT4(0.3f, 0.3f, 0.3f, 1.0f);
+//        materialProperties2.diffuse = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+//        materialProperties2.specular = DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+//        materialProperties2.shininess = 128.0f;
+//        graphics->UpdateMaterialProperties(materialCubeInner2.resource, materialProperties2);
 //
-//        GCLIGHT directionalLight;
-//        directionalLight.position = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
-//        directionalLight.direction = DirectX::XMFLOAT3(1.0f, -1.0f, -1.0f);
-//        directionalLight.color = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f);
-//        directionalLight.spotAngle = 0.0f;
-//        directionalLight.lightIntensity = 1.3f;
-//        directionalLight.lightType = LIGHT_TYPE_DIRECTIONAL;
+//        std::vector<GCLIGHT> lights;
+//
+//        //GCLIGHT light2;
+//        //light2.position = DirectX::XMFLOAT3(0.0f, 0.0f, 8.0f); // Position en 2D (x, y, 0)
+//        //light2.direction = DirectX::XMFLOAT3(0.0f, 0.0f, -1.0f); // Direction vers le bas en 2D
+//        //light2.color = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f); // Couleur de la lumière
+//        //light2.spotAngle = 30.0f; // Angle du spot en degrés (par exemple, 30 degrés)
+//        //light2.lightIntensity = 5.2f;
+//        //light2.lightType = LIGHT_TYPE_SPOT; // Type de lumière spot
+//
+//        GCLIGHT light1;
+//        light1.position = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f); // Position en 2D n'est pas utilisée pour les lumières directionnelles
+//        light1.direction = DirectX::XMFLOAT3(0.0f, -1.0f, 0.0f); // Direction vers le bas en 2D
+//        light1.color = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f); // Couleur de la lumière
+//        light1.spotAngle = 0.0f; // Non utilisé pour les lumières directionnelles
+//        light1.lightIntensity = 1.0f; // Intensité de la lumière
+//        light1.lightType = LIGHT_TYPE_DIRECTIONAL; // Type de lumière directionnelle
 //
 //        GCLIGHT pointLight;
-//        pointLight.position = DirectX::XMFLOAT3(0.4f, 0.4f, 0.0f); // Position en 2D (x, y, 0)
-//        pointLight.direction = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
+//        pointLight.position = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f); // Position en 2D (x, y, 0)
+//        pointLight.direction = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f); // Direction vers le bas en 2D
 //        pointLight.color = DirectX::XMFLOAT3(1.0f, 1.0f, 1.0f); // Couleur de la lumière
 //        pointLight.spotAngle = 0.0f; // Angle du spot si applicable
-//        pointLight.lightIntensity = 7.4f;
-//        pointLight.lightType = LIGHT_TYPE_POINT; // Type de lumière ponctuelle
+//        pointLight.lightIntensity = 20.4f;
+//        pointLight.lightType = 2; // Type de lumière ponctuelle
 //
+//        //lights.push_back(directionalLight);
+//        //lights.push_back(pointLight);
+//        lights.push_back(light1);
 //
-//        lightData.lights[1] = pointLight;
-//        //lightData.lights[0] = directionalLight;
-//
-//        graphics->UpdateLights(lightData);
+//        graphics->UpdateLights(lights);
 //
 //        graphics->StartFrame();
-//        graphics->UpdateViewProjConstantBuffer(storedProjectionMatrix, storedViewMatrix);
 //
-//        graphics->UpdateWorldConstantBuffer(materialSphere.resource, worldCubeInner2, 2.0f);
-//        graphics->GetRender()->DrawObject(meshSphere.resource, materialSphere.resource, true);
 //
+//        graphics->UpdateWorldConstantBuffer(materialCubeInner.resource, worldCubeInner2, 2.0f);
+//        graphics->GetRender()->DrawObject(meshSphere.resource, materialCubeInner.resource, true);
+//
+//        //graphics->UpdateWorldConstantBuffer(materialCubeInner2.resource, worldCubeInner, 2.0f);
+//        //graphics->GetRender()->DrawObject(meshSphere.resource, materialCubeInner2.resource, true);
 //
 //        graphics->EndFrame();
 //        window->Run(graphics->GetRender());
 //    }
+//
+//    delete graphics;
 //
 //    return 0;
 //}
