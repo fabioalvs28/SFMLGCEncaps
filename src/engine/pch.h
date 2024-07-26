bbdda23a7caa52db368397de5db9c485940eae0f
Mateo-Lemercier <mlemercier@gaming.tech>
2024-07-26 10:53:44 +0200

    [ADD] pch.h in Core & Engine

--- src/engine/pch.h
+++ src/engine/pch.h
@@ -9,18 +9,83 @@
 
 typedef const char* cstr;
 typedef const wchar_t* custr;
-typedef __int8                I8;
-typedef __int16                I16;
-typedef __int32                I32;
-typedef __int64                I64;
-typedef unsigned __int8        UI8;
-typedef unsigned __int16    UI16;
-typedef unsigned __int32    UI32;
-typedef unsigned __int64    UI64;
+typedef __int8 I8;
+typedef __int16 I16;
+typedef __int32 I32;
+typedef __int64 I64;
+typedef unsigned __int8 UI8;
+typedef unsigned __int16 UI16;
+typedef unsigned __int32 UI32;
+typedef unsigned __int64 UI64;
 
 
-// add headers that you want to pre-compile here
-#include "framework.h"
-#include "GCColor.h"
+
+
+
+
+// Singleton
+
+class GC;
+
+
+
+// Managers
+
+class GCGameManager;
+class GCTime;
+
+class GCInputSystem;
+class GCInputManager;
+class GCKeyboardInputManager;
+class GCMouseInputManager;
+// class GCControllerManager;
+// class GCControllerInputManager;
+
+class GCEventManager;
+class GCPhysicManager;
+class GCUpdateManager;
+class GCSceneManager;
+class GCRenderManager;
+
+
+
+// Gameplay classes
+
+class GCScene;
+class GCGameObject;
+
+class Component;
+class SpriteRenderer;
+class Collider;
+class BoxCollider;
+class CircleCollider;
+class RigidBody;
+class Animator;
+class SoundMixer;
+class Camera;
+class Script;
+
+class GCGameObjectTransform;
+
+
+
+
+
+
+#include "GC.h"
+
+#include "GameManager.h"
+#include "Timer.h"
+#include "InputManager.h"
+#include "EventManager.h"
+#include "PhysicManager.h"
+#include "UpdateManager.h"
+#include "SceneManager.h"
+#include "RenderManager.h"
+
+#include "Scene.h"
+#include "GameObject.h"
+#include "Components.h"
+#include "GameObjectTransform.h"
 
 #endif //ENGINE_PCH_H
