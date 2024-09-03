299116504c0c7723495dd2adf76e3b1e6f70cda1
Kaktousse <elia.maillardsalin@gmail.com>
2024-09-03 17:26:23 +0200

    [ADD] Sprites

--- bin/SpriteSheetGenerator-py/runSpriteGenCPP.bat
+++ bin/SpriteSheetGenerator-py/runSpriteGenCPP.bat
@@ -1,1 +1,1 @@
-..\..\ide\vs\SpriteSheetGenerator\Build\debug\spriteSheetGenerator.exe -i ../../res/player -o ../../res/spritesheet -p 0 -R -F false -T
+..\..\ide\vs\SpriteSheetGenerator\Build\debug\spriteSheetGenerator.exe -i ../../res/goats/DARK -o ../../res/spritesheet -p 0 -R -F false -T
